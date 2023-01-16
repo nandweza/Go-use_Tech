286397d505bf669202363af053c7b1e935219b99
nandweza <allannandweza@gmail.com>
2023-01-16 13:39:59 +0300

    connecting app to mongodb

--- Backend/index.js
+++ Backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
-
 const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv");
 
-const port = 4001;
+const port = 3000;
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connected Successfully!"))
+  .catch((err) => console.log(err));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     res.send('Welcome to Go-use Tech!');
