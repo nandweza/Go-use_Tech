42bd673c04c5211192794dd1510452af6edf36db
nandweza <allannandweza@gmail.com>
2023-02-24 19:04:23 +0300

    created contact backend

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -9,6 +9,7 @@ const User = require('../models/User');
 const Course = require('../models/Course');
 const Post = require('../models/Post');
 const fs = require('fs');
+const nodemailer = require('nodemailer');
 
 // const initializeApp = require('firebase/app');
 // const getStorage =  require('firebase/storage');
@@ -38,6 +39,40 @@ const upload = multer({ storage: Storage }).single('img');
 //home routes
 router.get('/', (req, res) => {
     res.render('home');
+});
+
+//contact
+router.post('/contact', (req, res) => {
+    console.log(req.body);
+    // const name = req.body.name;
+    // const email = req.body.email;
+    // const subject = req.body.subject;
+    // const message = req.body.message;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'allannandweza@gmail.com',
+            pass: 'raak arbd cizr rlpw'
+        }
+    });
+    
+    const mailOptions = {
+        from: req.body.email,
+        to: 'allannandweza@gmail',
+        suject: `Message from ${req.body.email}: ${req.body.subject}`,
+        text: req.body.message
+    }
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.log(error);
+            res.send('error');
+        } else {
+            console.log('Email sent: ' + info.response);
+            res.send('success');
+        }
+    })
 });
 
 /* ----- register routes ----- */
